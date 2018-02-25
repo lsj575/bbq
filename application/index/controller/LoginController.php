1c33e31cfb1694962b25fe1ef2a4c46a16091002
sijie.long <768712778@qq.com>
2018-02-25 15:07:59 +0800

    登录部分代码优化，减少重复部分

--- application/index/controller/LoginController.php
+++ application/index/controller/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\controller;
 
+use app\index\model\Member;
 use think\Controller;
 use think\Session;
 
@@ -14,8 +15,14 @@ class LoginController extends Controller
     public function msign()
     {
         $info = json_decode(urldecode($_SERVER['HTTP_M_SIGN']), true);
+        if(!$info) exit('授权被拒绝');
 
         Session::set('user', $info['cardNo']);
+
+        $return = $this->login($info);
+
+        return apireturn($return['code'], $return['msg'], $return['data'], 200);
+
     }
 
     private function check($info)
@@ -35,6 +42,25 @@ class LoginController extends Controller
     {
         $info = json_decode($_POST['user'], true);
         if(!$info || empty($info['cardno'])) exit('授权被拒绝');
-        var_dump($info);
+
+        Session::set('user', $info['cardno']);
+
+        $return = $this->login();
+
+        return apireturn($return['code'], $return['msg'], $return['data'], 200);
+    }
+
+    public function login($info)
+    {
+        $user = new Member();
+
+        $member = $user->FindOneUser($info['cardno']);
+        if(is_null($member['data'])) {
+            $res = $user->InsertOneUser($info);
+            return $res;
+        } else {
+            $res = $user->UpdateLoginTime($member['data']['id'], $member['data']['login']);
+            return $res;
+        }
     }
 }
