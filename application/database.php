7ebeb5ef773be81fe8753b223bb9f65e955ba4fe
sijie.long <768712778@qq.com>
2018-05-05 00:01:21 +0800

    修改数据库名和密码

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => '',
+    'database'        => 'bbq',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => 'token123',
     // 端口
     'hostport'        => '',
     // 连接dsn
