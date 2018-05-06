6bb7cd906b95ad882ac2aa2444641e9f83625f76
sijie.long <768712778@qq.com>
2018-05-07 00:01:16 +0800

    增加对登陆的验证参数verify，和学号。以及相应的校验函数

--- application/index/controller/LoginController.php
+++ application/index/controller/LoginController.php
@@ -8,6 +8,9 @@ use think\Session;
 
 class LoginController extends Controller
 {
+    private static $key = 'tokencoursetable';
+    private static $iv = 'TOKEN123token123';
+
     public function index()
     {
         var_dump(Session::get('user'));
@@ -61,34 +64,72 @@ class LoginController extends Controller
     {
         $postData = Request::instance()->post();
 
-        $user = new User();
+        $isLogin = $this->checkIsLogin($postData);
+        if ($isLogin) {
+            $user = new User();
 
-        $theUser = $user->findOneUser($postData['cardno']);
-        if(is_null($theUser['data'])) {
-            $res = $user->insertOneUser($postData);
-            if ($res['code'] == 0) {
-                $userinfo = $user->findOneUser($postData['cardno']);
+            $theUser = $user->findOneUserByCardno($postData['cardno']);
+            if(is_null($theUser['data'])) {
+                $sessionID = session_id();
+                $time = time();
+                $token = json_encode($theUser['data']['id']) . '|' .$sessionID . '|' . $time;
+                $postData['token'] = base64_encode($token);
+                $res = $user->insertOneUser($postData);
+                if ($res['code'] == 0) {
+                    $userinfo = $user->findOneUserByCardno($postData['cardno']);
+                    $this->setSession($userinfo['data']);
+
+
+                    $userinfo['data']['token'] = base64_encode($token);
+                    return apireturn($userinfo['code'], $userinfo['msg'], $userinfo['data'], 200);
+                } else {
+                    return apireturn($res['code'], $res['msg'], null, 500);
+                }
+
+            } else {
+                $this->setSession($theUser['data']);
+
+                $sessionID = session_id();
+                $time = time();
+                $token = json_encode($theUser['data']['id']) . '|' .$sessionID . '|' . $time;
+                $theUser['data']['token'] = base64_encode($token);
+                $res = $user->updateLogin($theUser['data']['id'], $theUser['data']['token']);
+
+                return apireturn($theUser['code'], $theUser['msg'], $theUser['data'], 200);
             }
-
-            $this->setSession($userinfo['data']);
-            $userinfo['data']['session_id'] = session_id();
-
-            return apireturn($userinfo['code'], $userinfo['msg'], $userinfo['data'], 200);
         } else {
-            $res = $user->updateLoginTime($theUser['data']['id']);
-
-            $this->setSession($theUser['data']);
-            $theUser['data']['session_id'] = session_id();
-
-            return apireturn($theUser['code'], $theUser['msg'], $theUser['data'], 200);
+            return apireturn(10004, "Login verification failed", null, 200);
         }
+
+
+    }
+
+    public function checkIsLogin($data) {
+        $verify = base64_decode($data['verify']);
+        $verification = explode("|", $verify);
+        $sno = self::decrypt($verification[1]);
+        if ($data['cardno'] == $verification[0] && $data['sno'] == $sno) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function encrypt($str) {
+        $str = $str.str_repeat("\0", 16 - strlen($str) % 16);
+        $encrypt = openssl_encrypt($str, 'AES-128-CBC', self::$key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING, self::$iv);
+
+        return bin2hex($encrypt);
+    }
+
+    public static function decrypt($str) {
+        $decrypt = openssl_decrypt(hex2bin($str), 'AES-128-CBC', self::$key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING, self::$iv);
+        return rtrim($decrypt, "\0");
     }
 
     public function setSession($data)
     {
         Session::set('user.id', $data['id']);
-        Session::set('user.cardno', $data['cardno']);
-        Session::set('user.nickname', $data['nickname']);
     }
 
     public function deleteSession()
