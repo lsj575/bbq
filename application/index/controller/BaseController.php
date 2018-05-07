fe454fee3a671da58f76f3b7b638e9873950ec5d
sijie.long <768712778@qq.com>
2018-05-07 12:40:43 +0800

    修改调用接口验证方式

--- application/index/controller/BaseController.php
+++ application/index/controller/BaseController.php
@@ -3,29 +3,50 @@ namespace app\index\controller;
 
 use think\Controller;
 use app\index\controller\LoginController;
+use think\Request;
+use think\Session;
+use app\index\model\User;
 
 class BaseController extends Controller
 {
     public function index()
     {
-        return $this->fetch();
+        return 1;
     }
 
-    protected function _initialize()
-    {
-        if (!isset($_SESSION['user'])){
-            $this->login();
-        }
-    }
-
-    protected function login()
+    public function checkToken()
     {
         if (isset($_SERVER['HTTP_M_SIGN'])) {
             $login = new LoginController();
             $res = $login->msign();
             $this->redirect('http://test.wutnews.net/lsj/bbq/public/index/login/index');
+        } elseif (isset($_SERVER['HTTP_BSIGN'])){
+            if ($_SERVER['HTTP_BSIGN'] == 'APP') {
+                $postData = Request::instance()->post('token');
+                $token = base64_decode($postData);
+                $data = explode("|", $token);
+
+                $user = new User();
+                $res = $user->findOneUserById($data[0]);
+
+                if ($data[2] + 8640000 < time()) {
+                    return 10011;
+                } elseif ($res['data']['token'] != $postData) {
+                    return 10012;
+                } else {
+                    return 0;
+                }
+            } elseif ($_SERVER['HTTP_BSIGN'] == 'WEB') {
+                $postData = Request::instance()->post('token');
+                $token = base64_decode($postData);
+                $data = explode("|", $token);
+
+                Session::set('user.id', $data[0]);
+            }
         } else {
-            return apireturn(10010, "User is not logged in.", null, 200);
+            return 10010;
         }
+
     }
+
 }
