185245212a5bd6105139c9af2496fc5ce50ef30e
sijie.long <768712778@qq.com>
2018-05-07 23:55:35 +0800

    新增修改用户昵称和修改用户简介接口

--- application/index/controller/UserController.php
+++ application/index/controller/UserController.php
@@ -22,7 +22,7 @@ class UserController extends BaseController
         $postData = Request::instance()->post();
 
         $user = new User();
-        $res = $user->findOneUserById($postData['id']);
+        $res = $user->findOneUserById($code);
 
         if (is_null($res)) {
             return apireturn($res['code'], $res['msg'], null, 200);
@@ -30,5 +30,49 @@ class UserController extends BaseController
             unset($res['data']['token']);
             return apireturn($res['code'], $res['msg'], $res['data'], 200);
         }
+    }
+
+    public function changeUserNickname()
+    {
+        $code = parent::checkToken();
+
+        if ($code == 10010) {
+            return apireturn(10010, "User is not logged in.", null, 200);
+        } elseif ($code == 10011) {
+            return apireturn(10011, "Landing expired.", null, 200);
+        } elseif ($code == 10012) {
+            return apireturn(10012, "Invalid login token.", null, 200);
+        }
+        $postData = Request::instance()->post();
+
+        $user = new User();
+
+        $res = $user->changeUserNicknameById($code, $postData['nickname']);
+
+        if ($res['code'] == 0) {
+            return apireturn(0, "Update user nickname success.", null, 200);
+        } else {
+            return apireturn($res['code'], "Failed to update user nickname.", null, 200);
+        }
+    }
+
+    public function changeUserIntroduction()
+    {
+        $code = parent::checkToken();
+
+        if ($code == 10010) {
+            return apireturn(10010, "User is not logged in.", null, 200);
+        } elseif ($code == 10011) {
+            return apireturn(10011, "Landing expired.", null, 200);
+        } elseif ($code == 10012) {
+            return apireturn(10012, "Invalid login token.", null, 200);
+        }
+        $postData = Request::instance()->post();
+
+        $user = new User();
+
+        $res = $user->changeUserIntroductionById($code, $postData['introduction']);
+
+        return apireturn($res['code'], $res['msg'], null, 200);
     }
 }
