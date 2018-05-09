27ff247fb73271f8fb3e481f10a899a1289c2189
sijie.long <768712778@qq.com>
2018-05-09 12:31:40 +0800

    修改返回值，对getuserinfo方法中对头像和主页图的返回数据做处理

--- application/index/controller/UserController.php
+++ application/index/controller/UserController.php
@@ -7,16 +7,19 @@ use app\index\model\User;
 
 class UserController extends BaseController
 {
+    private $avatarURL = 'http://127.0.0.1/gitlab/bbq/uploads/user_avatar/';
+    private $homeImgURL = 'http://127.0.0.1/gitlab/bbq/uploads/home_img/';
+
     public function getUserInfo()
     {
         $code = parent::checkToken();
 
-        if ($code == 10010) {
-            return apireturn(10010, "User is not logged in.", null, 200);
-        } elseif ($code == 10011) {
-            return apireturn(10011, "Landing expired.", null, 200);
-        } elseif ($code == 10012) {
-            return apireturn(10012, "Invalid login token.", null, 200);
+        if ($code == 10100) {
+            return apireturn(10100, "User is not logged in.", null, 200);
+        } elseif ($code == 10101) {
+            return apireturn(10101, "Landing expired.", null, 200);
+        } elseif ($code == 10102) {
+            return apireturn(10102, "Invalid login token.", null, 200);
         }
 
         $postData = Request::instance()->post();
@@ -24,10 +27,14 @@ class UserController extends BaseController
         $user = new User();
         $res = $user->findOneUserById($code);
 
-        if (is_null($res)) {
+        if (is_null($res['data'])) {
             return apireturn($res['code'], $res['msg'], null, 200);
         } else {
             unset($res['data']['token']);
+            $res['data']['avatar'] = str_replace('\\', '/', $res['data']['avatar']);
+            $res['data']['home_img'] = str_replace('\\', '/', $res['data']['home_img']);
+            $res['data']['avatar'] = $this->avatarURL . $res['data']['avatar'];
+            $res['data']['home_img'] = $this->homeImgURL . $res['data']['home_img'];
             return apireturn($res['code'], $res['msg'], $res['data'], 200);
         }
     }
@@ -36,12 +43,12 @@ class UserController extends BaseController
     {
         $code = parent::checkToken();
 
-        if ($code == 10010) {
-            return apireturn(10010, "User is not logged in.", null, 200);
-        } elseif ($code == 10011) {
-            return apireturn(10011, "Landing expired.", null, 200);
-        } elseif ($code == 10012) {
-            return apireturn(10012, "Invalid login token.", null, 200);
+        if ($code == 10100) {
+            return apireturn(10100, "User is not logged in.", null, 200);
+        } elseif ($code == 10101) {
+            return apireturn(10101, "Landing expired.", null, 200);
+        } elseif ($code == 10102) {
+            return apireturn(10102, "Invalid login token.", null, 200);
         }
         $postData = Request::instance()->post();
 
@@ -60,12 +67,12 @@ class UserController extends BaseController
     {
         $code = parent::checkToken();
 
-        if ($code == 10010) {
-            return apireturn(10010, "User is not logged in.", null, 200);
-        } elseif ($code == 10011) {
-            return apireturn(10011, "Landing expired.", null, 200);
-        } elseif ($code == 10012) {
-            return apireturn(10012, "Invalid login token.", null, 200);
+        if ($code == 10100) {
+            return apireturn(10100, "User is not logged in.", null, 200);
+        } elseif ($code == 10101) {
+            return apireturn(10101, "Landing expired.", null, 200);
+        } elseif ($code == 10102) {
+            return apireturn(10102, "Invalid login token.", null, 200);
         }
         $postData = Request::instance()->post();
 
@@ -74,5 +81,62 @@ class UserController extends BaseController
         $res = $user->changeUserIntroductionById($code, $postData['introduction']);
 
         return apireturn($res['code'], $res['msg'], null, 200);
+    }
+
+    public function uploadUserAvatar()
+    {
+        $code = parent::checkToken();
+
+        if ($code == 10100) {
+            return apireturn(10100, "User is not logged in.", null, 200);
+        } elseif ($code == 10101) {
+            return apireturn(10101, "Landing expired.", null, 200);
+        } elseif ($code == 10102) {
+            return apireturn(10102, "Invalid login token.", null, 200);
+        }
+
+        $file = request()->file('avatar');
+        if ($file){
+            // 移动到框架应用根目录/public/uploads/ 目录下
+            $info = $file->move(ROOT_PATH . DS  . 'uploads'. DS . 'user_avatar', true, false);
+            if($info){
+                $savaName = $info->getSaveName();
+
+                $user = new User();
+                $res = $user->uploadUserAvatar($code, $savaName);
+                return apireturn($res['code'], $res['msg'], null, 200);
+            }else{
+                // 上传失败获取错误信息
+                return apireturn(20001, 'Picture upload failed', null, 200);
+            }
+        }
+    }
+
+    public function uploadUserHomeImg()
+    {
+        $code = parent::checkToken();
+
+        if ($code == 10100) {
+            return apireturn(10100, "User is not logged in.", null, 200);
+        } elseif ($code == 10101) {
+            return apireturn(10101, "Landing expired.", null, 200);
+        } elseif ($code == 10102) {
+            return apireturn(10102, "Invalid login token.", null, 200);
+        }
+
+        $file = request()->file('home_img');
+        if ($file){
+            $info = $file->move(ROOT_PATH . DS . 'uploads'. DS . 'home_img', true, false);
+            if($info){
+                $savaName = $info->getSaveName();
+
+                $user = new User();
+                $res = $user->uploadUserHomeImg($code, $savaName);
+                return apireturn($res['code'], $res['msg'], null, 200);
+            }else{
+                // 上传失败获取错误信息
+                return apireturn(20001, 'Picture upload failed', null, 200);
+            }
+        }
     }
 }
