66352f5491d1eda997cd23fb503d285c3613bc20
sijie.long <768712778@qq.com>
2018-05-10 23:12:42 +0800

    【修改】图片上传与访问路径 【新增】获取所有主体信息方法

--- application/index/controller/ThemeController.php
+++ application/index/controller/ThemeController.php
@@ -7,7 +7,9 @@ use think\Session;
 
 class ThemeController extends BaseController
 {
-    private $themeBackgroudImgURL = 'http://127.0.0.1/gitlab/bbq/uploads/theme_img/';
+    //private $themeBackgroudImgURL = 'http://127.0.0.1/gitlab/bbq/public/static/uploads/theme_img/';
+    private $themeBackgroudImgURL = 'http://bbq.wutnews.net/bbq/public/static/uploads/theme_img/';
+
     public function addTheme()
     {
         $code = parent::checkToken();
@@ -23,23 +25,23 @@ class ThemeController extends BaseController
         $postData = Request::instance()->post();
 
         $file = Request::instance()->file('themeimg');
-        if ($file){
+        if ($file) {
             // 移动到框架应用根目录/public/uploads/ 目录下
-            $info = $file->move(ROOT_PATH . DS . 'uploads'. DS . 'theme_img', true, false);
-            if($info){
+            $info = $file->move(ROOT_PATH . DS . 'public' . DS . 'static' . DS . 'uploads' . DS . 'theme_img', true, false);
+            if ($info) {
                 $postData['userID'] = $code;
                 $postData['path'] = $info->getSaveName();
                 $theme = new Theme();
                 $res = $theme->addTheme($postData);
                 return apireturn($res['code'], $res['msg'], $res['data'], 200);
-            }else{
+            } else {
                 // 上传失败获取错误信息
                 return apireturn(20001, 'Picture upload failed', null, 200);
             }
         }
     }
 
-    public function findThemeByUserId()
+    public function findUserCreateThemeByUserId()
     {
         $code = parent::checkToken();
 
@@ -59,6 +61,33 @@ class ThemeController extends BaseController
         } else {
             $res['data']['background_img'] = str_replace('\\', '/', $res['data']['background_img']);
             $res['data']['background_img'] = $this->themeBackgroudImgURL . $res['data']['background_img'];
+            return apireturn($res['code'], $res['msg'], $res['data'], 200);
+        }
+    }
+
+    public function getAllTheme()
+    {
+        $code = parent::checkToken();
+
+        if ($code == 10100) {
+            return apireturn(10100, "User is not logged in.", null, 200);
+        } elseif ($code == 10101) {
+            return apireturn(10101, "Landing expired.", null, 200);
+        } elseif ($code == 10102) {
+            return apireturn(10102, "Invalid login token.", null, 200);
+        }
+
+        $theme = new Theme();
+        $res = $theme->findAllTheme();
+
+        if (is_null($res['data'])) {
+            return apireturn($res['code'], $res['msg'], null, 200);
+        } else {
+            var_dump($res['data'][0]['theme_name']);
+            foreach ($res['data'] as $key => $value) {
+                $res['data'][$key]['background_img'] = str_replace('\\', '/', $res['data'][$key]['background_img']);
+                $res['data'][$key]['background_img'] = $this->themeBackgroudImgURL . $res['data'][$key]['background_img'];
+            }
             return apireturn($res['code'], $res['msg'], $res['data'], 200);
         }
     }
