a9db702a8b9181694ddebd5b2e9b11e58b2e7604
sijie.long <768712778@qq.com>
2018-07-02 16:53:23 +0800

    【新增】IndexController控制器，输出前端页面

---
+++ application/admin/controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class IndexController extends  Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function welcome()
+    {
+        return $this->fetch();
+    }
+}
