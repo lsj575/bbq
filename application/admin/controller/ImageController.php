c1fb45a46af599ca8e466881a85e7d38aebd87b4
sijie.long <768712778@qq.com>
2018-07-04 16:44:25 +0800

    【新增】图片上传插件

---
+++ application/admin/controller/ImageController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 76871
+ * Date: 2018/7/4
+ * Time: 16:23
+ */
---
+++ public/static/admin/js/image.js
@@ -0,0 +1,7 @@
+// 使用uploadify的dome
+$(function() {
+    $("#file_upload").uploadify({
+        swf           : swf,
+        uploader      : image_upload_url,
+    });
+});
