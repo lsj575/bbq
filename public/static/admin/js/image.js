fa1ba6f44051435229d5794a0468efde065d32c1
sijie.long <768712778@qq.com>
2018-07-04 17:31:15 +0800

    【修改】模板文件中的插件

--- public/static/admin/js/image.js
+++ public/static/admin/js/image.js
@@ -3,5 +3,18 @@ $(function() {
     $("#file_upload").uploadify({
         swf           : swf,
         uploader      : image_upload_url,
+        buttonText    : '图片上传',
+        fileTypeDesc  : 'Image files',
+        fileObjName   : 'file',
+        fileTypeExts  : '*.gif;*.jpg;*.png',
+        onUploadSuccess : function (file, data, response) {
+            if (response) {
+                var obj = JSON.parse(data);
+                $('#upload_org_code_img').attr("src", obj.data);
+                $('#file_upload_image').attr("value", obj.data);
+                $('#upload_org_code_img').show();
+            }
+        }
+        
     });
 });
