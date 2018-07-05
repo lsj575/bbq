5c67c4cdf4b3bf03e1f0fa7a5b951d936d2479be
sijie.long <768712778@qq.com>
2018-07-05 18:15:21 +0800

    feat: 增加图片上传方法

--- application/admin/controller/ImageController.php
+++ application/admin/controller/ImageController.php
@@ -1,7 +1,36 @@
 <?php
+namespace app\admin\controller;
+
+use think\Controller;
+use think\Request;
+
 /**
- * Created by PhpStorm.
- * User: 76871
- * Date: 2018/7/4
- * Time: 16:23
+ * Class ImageController
+ * @package app\admin\controller
+ * 后台图片上传逻辑
  */
+class ImageController extends BaseController
+{
+    /**
+     * 图片上传
+     */
+    public function upload()
+    {
+        $file = Request::instance()->file('file');
+        // 把图片上传到指定的文件夹
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+
+        //TODO 将返回方式进行封装，将图片路径修改为url的形式
+        if ($info && $info->getSaveName()){
+            $data = [
+                'status' => 1,
+                'message'=> 'OK',
+                'data'   => $info->getSaveName(),
+            ];
+            echo json_encode($data);
+        }else {
+            echo json_encode(['status' => 0, 'message' => '上传失败']);
+        }
+
+    }
+}
