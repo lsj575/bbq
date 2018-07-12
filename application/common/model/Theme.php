e0733e5047454967fd6202ad9907ea487d534fb6
sijie.long <768712778@qq.com>
2018-07-12 11:17:31 +0800

    feat：theme模型新增getTheme方法，实现自动分页

--- application/common/model/Theme.php
+++ application/common/model/Theme.php
@@ -4,5 +4,24 @@ namespace app\common\model;
 use think\Model;
 class Theme extends Base
 {
+    /**
+     * 查询主题 后台自动分页
+     * @param array $data
+     * @return \think\Paginator
+     */
+    public function getTheme($data = [])
+    {
+        $data['status'] = [
+            'neq', config('code.status_delete')
+        ];
 
+        $order = ['id' => 'desc'];
+
+        // 查询
+        $result = $this->where($data)
+            ->order($order)
+            ->paginate();
+
+        return $result;
+    }
 }
