a26d5ff06b815844eec3c832329dda8cb4cb2191
sijie.long <768712778@qq.com>
2018-07-12 11:23:25 +0800

    feat：新增将模板中的数字转为对应文字的函数

--- application/common.php
+++ application/common.php
@@ -10,3 +10,22 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+function pagination($obj)
+{
+    if (!$obj) {
+        return '';
+    }
+
+    $params = request()->param();
+    return '<div class="bbq-app">'.$obj->appends($params)->render().'</div>';
+}
+
+function isPosition($str)
+{
+    return $str ? '<span style="color:red"> 是</span>' : '<span > 否</span>';
+}
+function getStatusName($status)
+{
+    return $status ? '<span> 已发布</span>' : '<span > 待审核</span>';
+}
