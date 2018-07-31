72b57b96cac4fae238e1564ba73aea4c48d4be43
sijie.long <768712778@qq.com>
2018-07-31 12:18:38 +0800

    style：删除冗余代码

--- application/admin/controller/ThemeController.php
+++ application/admin/controller/ThemeController.php
@@ -47,8 +47,8 @@ class ThemeController extends BaseController
             // 获取session
             $user = session(config('admin.session_user'), '', config('admin.session_user_scope'));
             // form表单key名转换
-            (!isset($data['is_position']))? $data['is_position'] = 0: $data['is_position'] = 1;
-            (!isset($data['is_head_figure']))? $data['is_head_figure'] = 0: $data['is_head_figure'] = 1;
+            (!isset($data['is_position'])) ? $data['is_position'] = 0 : $data['is_position'] = 1;
+            (!isset($data['is_head_figure']))? $data['is_head_figure'] = 0 : $data['is_head_figure'] = 1;
             //TODO 将图片路径改为http：//的格式
             $data = array(
                 'user_id'             => $user->id,
@@ -58,7 +58,7 @@ class ThemeController extends BaseController
                 'is_position'         => $data['is_position'],
                 'is_head_figure'      => $data['is_head_figure'],
             );
-            // var_dump($data);
+
             //入库操作
             try {
                 $id = model('Theme')->add($data);
@@ -67,7 +67,7 @@ class ThemeController extends BaseController
             }
 
             if ($id) {
-                return $this->result(['jump_url' =>url('theme/index')], config('code.SUCCESS'), 'OK');
+                return $this->result(['jump_url' => url('theme/index')], config('code.SUCCESS'), 'OK');
             }else {
                 return $this->result('', config('code.FAILURE'), '新增失败');
             }
