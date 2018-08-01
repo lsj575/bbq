3ad494d2ab72d3705bf6861cb00a5ad5894a7796
sijie.long <768712778@qq.com>
2018-08-01 11:37:38 +0800

    feat：新增用户，来源，返回值等多种状态码

--- application/extra/code.php
+++ application/extra/code.php
@@ -4,9 +4,20 @@
  * 和状态码相关的文案配置
  */
 return [
-    'status_delete'   => -1,
-    'status_normal'   => 1,
-    'status_padding'  => 0,  //待审核
-    'SUCCESS'         => 1,  //成功
-    'FAILURE'         => 0,  //失败
+    'status_delete'         => -1,
+    'status_normal'         => 1,
+    'status_padding'        => 0,  //待审核
+    'SUCCESS'               => 1,  //成功
+    'FAILURE'               => 0,  //失败
+    'source_admin'          => 0,  //来自admin用户
+    'source_user'           => 1,  //来自普通用户
+    'source_forward'        => 2,  //通过转发
+    'app_show_success'      => 1,
+    'app_show_error'        => 0,
+    'article_type_original' => 0,  //原创文章
+    'article_type_forward'  => 1,  //转发文章
+    'user_normal'           => 1,  //用户正常
+    'user_delete'           => -1,  //用户被删除
+    'user_ban'              => 0,  //用户被禁止
+
 ];
