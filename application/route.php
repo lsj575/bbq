4b0801338694cd43968e66cd7056ed51b80122c1
sijie.long <768712778@qq.com>
2018-08-17 14:44:55 +0800

    feat:新增多条路由

--- application/route.php
+++ application/route.php
@@ -1,21 +1,53 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+use think\Route;
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
 
-];
+/**
+ * 主题相关路由
+ */
+
+//获取所有主题
+Route::get('api/:ver/theme', 'api/:ver.theme/getAllTheme');
+
+
+/**
+ * 推荐页相关
+ */
+Route::get('api/:ver/index', 'api/:ver.article/getIndexHeadNormalNews');
+
+/**
+ * 文章相关
+ */
+//文章评论
+Route::post('api/:ver/comment', 'api/:ver.comment/save');
+Route::get('api/:ver/comment/:id', 'api/:ver.comment/read');
+
+
+//登录路由
+Route::post('api/:ver/login', 'api/:ver.login/save');
+
+/**
+ * 用户信息相关
+ */
+//获取用户数据 【资源路由】
+/**
+ * 标识	  请求类型	生成路由规则	     对应操作方法（默认）
+ * index    GET	       blog	            index
+ * create   GET	       blog/create	    create
+ * save	    POST	   blog	            save
+ * read	    GET	       blog/:id	        read
+ * edit	    GET	       blog/:id/edit	edit
+ * update	PUT	       blog/:id	        update
+ * delete	DELETE	   blog/:id	        delete
+ */
+Route::resource('api/:ver/user', 'api/:ver.user');
+//检查用户昵称是否唯一
+Route::get('api/:ver/user/nickname/:id', 'api/:ver.user/checkUserNicknamePass');
+
+
+/**
+ * 图片相关路由
+ */
+//图片上传路由
+Route::post('api/:ver/image', 'api/:ver.image/save');
+
