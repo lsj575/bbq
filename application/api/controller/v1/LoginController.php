165221f58144f0d9fa900ed7a642ce1ac79375b6
sijie.long <768712778@qq.com>
2018-08-17 14:44:20 +0800

    feat:对数据库的方法添加异常处理

--- application/api/controller/v1/LoginController.php
+++ application/api/controller/v1/LoginController.php
@@ -11,6 +11,7 @@ use app\api\controller\CommonController;
 use app\common\lib\Aes;
 use app\common\lib\IAuth;
 use app\common\model\User;
+use think\Exception;
 
 class LoginController extends CommonController
 {
@@ -21,19 +22,19 @@ class LoginController extends CommonController
     public function save()
     {
         if (!request()->isPost()) {
-            return apiReturn(config('app_show_error'), '您没有权限', '', 403);
+            return apiReturn(config('code.app_show_error'), '您没有权限', '', 403);
         }
 
         $param = input('param.');
         // validate
         $validate = validate('Login');
         if (!$validate->check($param, 'Login.save')) {
-            return apiReturn(config('app_show_error'), $validate->getError(), '', 400);
+            return apiReturn(config('code.app_show_error'), $validate->getError(), '', 400);
         }
 
         $bool = $this->checkLoginVerify($param['verify'], $param['sno'], $param['cardno']);
         if (false == $bool) {
-            return apiReturn(config('app_show_error'), '非法请求', '', 400);
+            return apiReturn(config('code.app_show_error'), '非法请求', '', 400);
         }
 
         //获得token
@@ -52,14 +53,24 @@ class LoginController extends CommonController
                 'last_login_time' => time(),
                 'status' => 1,
             ];
-            $id = model('User')->add($data);
+            try {
+                $id = model('User')->add($data);
+            } catch (\Exception $e) {
+                return apiReturn(config('code.app_show_error'), $e->getMessage(), '', 500);
+            }
+
         } else {
             //非第一次登录，更新过期时间和token
             $data = [
                 'token' => $token,
                 'time_out' => strtotime("+" . config('app.login_time_out_day') . " days"),
             ];
-            $info = model('User')->save($data, ['id' => $user['id']]);
+            try {
+                $info = model('User')->save($data, ['id' => $user['id']]);
+            } catch (\Exception $e) {
+                return apiReturn(config('code.app_show_error'), $e->getMessage(), '', 500);
+            }
+
         }
 
         $obj = new Aes();
@@ -69,9 +80,9 @@ class LoginController extends CommonController
                 'token' => $obj->encrypt($token . "||" . $id . "||" . time()),
             ];
 
-            return apiReturn(config('app_show_success'), '登录成功', $result, 200);
+            return apiReturn(config('code.app_show_success'), '登录成功', $result, 200);
         } else {
-            return apiReturn(config('app_show_error'), '登录失败', '', 200);
+            return apiReturn(config('code.app_show_error'), '登录失败', '', 500);
         }
     }
 
