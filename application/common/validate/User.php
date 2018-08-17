9aa3c4f66e402c2c6069c10d64419ef078b351fb
sijie.long <768712778@qq.com>
2018-08-17 14:42:52 +0800

    feat:新增用户数据校验

---
+++ application/common/validate/User.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\common\validate;
+
+use think\Validate;
+
+class AdminUser extends Validate
+{
+    //提交规则，|号之间不能加多余符号，包括空格
+    protected $rule = [
+        'nickname'  => 'max:30',
+        'signature' => 'max:200',
+        'avatar'    => 'max:255',
+        'home_img'  => 'max:255',
+    ];
+
+    //应用场景
+    protected $scene = [
+        'update'   =>  ['nickname', 'signature', 'avatar', 'home_img'],
+        'checkUserNicknamePass' => ['nickname' => 'require|max:30',]
+    ];
+}
