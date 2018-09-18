8ae317b676b7c6436703d76a06cecd4e2b3a551a
sijie.long <768712778@qq.com>
2018-09-18 22:36:41 +0800

    feat:新增对accesstoken访问频率控制，和每次请求的入库记录操作

--- application/api/controller/v1/ImageController.php
+++ application/api/controller/v1/ImageController.php
@@ -9,6 +9,8 @@
 namespace app\api\controller\v1;
 
 use app\common\lib\Aes;
+use app\common\lib\exception\ApiException;
+use think\Cache;
 use think\Controller;
 
 class ImageController extends AuthBaseController
@@ -20,17 +22,65 @@ class ImageController extends AuthBaseController
     private $appCode = 'rjktdkjwgvkexmoujqaegficgcetscofgucldvqzwpxwwdfsxndpovuuvqethbwx';
 
     /**
+     * accessToken缓存过期时间
+     * @var int 3分钟
+     */
+    private $accessTokenCacheTimeOut = 150;
+
+    /**
      * 生成上传图片所需的accessToken
      * 由AppCode前32位数+时间戳去掉最后四位数+AppCode后32位数+随机Nonce+token123组成
      * @return string
      */
     public function getAccessToken()
     {
+        // 查询缓存中是否存在该用户已被禁止三分钟内请求accessToken
+        if (Cache::get("$this->user->id . 'accesstoken'"))
+        {
+            // 被禁止后，每次请求缓存值加一
+            Cache::inc("$this->user->id . 'accesstoken'");
+            return apiReturn(config('code.app_show_error'), '请求过于频繁，请三分钟后重试', '', 403);
+        }
+        // 查询数据库中的该用户请求次数
+        try {
+            $logCount = model('AccesstokenLog')->where('user_id', $this->user->id)
+                ->where('create_time', ['>', time()-60])
+                ->count();
+        } catch (\Exception $e) {
+            return apiReturn(config('code.app_show_error'), '数据库查询错误', '', 500);
+        }
+        // 若一分钟之内超过十次则提醒其等三分钟
+        if ($logCount >= 10)
+        {
+            // 将用户信息加入缓存中，禁止短时间内访问
+            Cache::set("$this->user->id . 'accesstoken'", 0, $this->accessTokenCacheTimeOut);
+            return apiReturn(config('code.app_show_error'), '请求过于频繁，请三分钟后重试', '', 403);
+        }
+
+        // 开始生成accessToken
         $nonce = $this->createNonce(16);
         $time = time() % 10000;
         $appCodeArray = str_split($this->appCode, 32);
         $accessToken = md5($appCodeArray[0] . $time . $appCodeArray[1] . $nonce . 'token123');
-        return apiReturn(config('code.app_show_success'), 'OK', $accessToken);
+
+        $data = [
+            'user_id'     => $this->user->id,
+            'source_type' => 0,
+            'accesstoken' => $accessToken,
+        ];
+
+        //入库操作
+        try {
+            $id =  model('AccesstokenLog')->add($data);
+        }catch (\Exception $e) {
+            return apiReturn(config('code.app_show_error'), '请求过于频繁，请三分钟后重试', '', 403);
+        }
+
+        if ($id) {
+            return apiReturn(config('code.app_show_success'), 'ok', $accessToken, 200);
+        }else {
+            return apiReturn(config('code.app_show_error'), '获取失败', '', 500);
+        }
     }
 
     /**
---
+++ application/common/model/AccesstokenLog.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\common\model;
+
+/**
+ * 记录信息控制器
+ * Class AppActive
+ * @package app\common\model
+ */
+class AccesstokenLog extends Base
+{
+    protected $table = 'accesstoken_log';
+
+}
