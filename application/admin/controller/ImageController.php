0cec109aa30492347600c38d99c3a8500c24eeb5
sijie.long <768712778@qq.com>
2018-10-06 22:13:48 +0800

    fix:修复返回数据异常的错误

--- application/admin/controller/ImageController.php
+++ application/admin/controller/ImageController.php
@@ -1,10 +1,6 @@
 <?php
 namespace app\admin\controller;
 
-use think\Controller;
-use think\Request;
-use think\Session;
-
 /**
  * Class ImageController
  * @package app\admin\controller
@@ -29,34 +25,38 @@ class ImageController extends BaseController
             // 获取session
             $user = session(config('admin.session_user'), '', config('admin.session_user_scope'));
             // 查询缓存中是否存在该用户已被禁止三分钟内请求accessToken
-            if (Cache::get($user->id . 'accesstoken'))
+            if (cache($user->id . 'accesstoken'))
             {
                 // 被禁止后，每次请求缓存值加一
-                Cache::inc($user->id . 'accesstoken');
-                return $this->result('', config('code.FAILURE'), '请三分钟后重试');
+                $count = cache($user->id . 'accesstoken');
+                cache($user->id . 'accesstoken', $count+1, $this->accessTokenCacheTimeOut);
+                return json(['data' => '', 'code' => config('code.FAILURE'), 'msg' => '请三分钟后重试']);
             }
 
             // 查询数据库中的该用户请求次数
             try {
-                $logCount = model('AccesstokenLog')->where('user_id', $this->user->id)
-                    ->where('create_time', ['>', time()-60])
+                // 获取session
+                $user = session(config('admin.session_user'), '', config('admin.session_user_scope'));
+
+                $logCount = model('AccesstokenLog')->where('user_id', $user->id)
+                    ->where('create_time', '>', time()-60)
                     ->count();
             } catch (\Exception $e) {
-                return $this->result('', config('code.FAILURE'), '数据库错误，请联系技术人员');
+                return json(['data' => '', 'code' => config('code.FAILURE'), 'msg' => '数据库错误请联系技术人员']);
             }
 
             // 若一分钟之内超过十次则提醒其等三分钟
             if ($logCount >= 10)
             {
                 // 将用户信息加入缓存中，禁止短时间内访问
-                Cache::set($user->id . 'accesstoken', 0, $this->accessTokenCacheTimeOut);
-                return $this->result('', config('code.FAILURE'), '请三分钟后重试');
+                cache($user->id . 'accesstoken', 0, $this->accessTokenCacheTimeOut);
+                return json(['data' => '', 'code' => config('code.FAILURE'), 'msg' => '请三分钟后重试']);
             }
 
             // 开始生成accessToken
             $nonce = $this->createNonce(16);
             $time = time() % 10000;
-            $appCodeArray = str_split(config('code.app_code'), 32);
+            $appCodeArray = str_split(config('code.APP_CODE'), 32);
             $accessToken = md5($appCodeArray[0] . $time . $appCodeArray[1] . $nonce . 'token123');
 
             $data = [
@@ -69,13 +69,13 @@ class ImageController extends BaseController
             try {
                 $id =  model('AccesstokenLog')->add($data);
             }catch (\Exception $e) {
-                return $this->result('', config('code.FAILURE'), '新增数据失败，请重试或联系技术人员');
+                return json(['data' => '', 'code' => config('code.FAILURE'), 'msg' => '新增数据失败，请重试或联系技术人员']);
             }
 
             if ($id) {
-                return $this->result($accessToken, config('code.SUCCESS'), '获取成功');
+                return json(['data' => ['acesstoken' => $accessToken], 'code' => config('code.SUCCESS'), 'msg' => 'OK']);
             }else {
-                return $this->result('', config('code.FAILURE'), '获取数据失败');
+                return json(['data' => '', 'code' => config('code.FAILURE'), 'msg' => '获取数据失败，请重试或联系技术人员']);
             }
         }
     }
