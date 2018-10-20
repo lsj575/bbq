9a717cd12d8a1f0d01d279ab60f2e3074e4d661a
sijie.long <768712778@qq.com>
2018-10-20 11:19:21 +0800

    feat:新增反馈模型和反馈类型模型

---
+++ application/common/model/Feedback.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 龙思杰
+ * Date: 2018/10/20
+ * Time: 11:17
+ */
+namespace app\common\model;
+
+class Feedback extends Base
+{
+    protected $table = 'feedback';
+}
--- application/common/model/FeedbackType.php
+++ application/common/model/FeedbackType.php
@@ -1,7 +1,14 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: 76871
+ * User: 龙思杰
  * Date: 2018/10/18
  * Time: 10:19
  */
+
+namespace app\common\model;
+
+class FeedbackType extends Base
+{
+    protected $table = 'feedback_type';
+}
