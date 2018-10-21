1809d33057970f5b68e8542f3681695fba3bd433
sijie.long <768712778@qq.com>
2018-10-21 18:37:04 +0800

    feat:新增后台获取反馈类型方法

--- application/common/model/FeedbackType.php
+++ application/common/model/FeedbackType.php
@@ -11,4 +11,25 @@ namespace app\common\model;
 class FeedbackType extends Base
 {
     protected $table = 'feedback_type';
+
+    /**
+     * 查询反馈类型 后台自动分页
+     * @param array $data
+     * @return \think\Paginator
+     */
+    public function getFeedbackType($data = [])
+    {
+        $data['status'] = [
+            'neq', config('code.status_delete')
+        ];
+
+        $order = ['id' => 'desc'];
+
+        // 查询
+        $result = $this->where($data)
+            ->order($order)
+            ->paginate();
+
+        return $result;
+    }
 }
