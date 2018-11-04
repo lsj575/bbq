1ed48f8bbce411b19eae7d9e0ce42f7a9d967e6e
sijie.long <768712778@qq.com>
2018-11-04 11:36:31 +0800

    fix:修复反馈页面查询错误误

--- application/admin/controller/FeedbackController.php
+++ application/admin/controller/FeedbackController.php
@@ -27,13 +27,13 @@ class FeedbackController extends BaseController
                 ['elt', strtotime($data['end_time'])],
             ];
         } else {
-            $whereData['create_time'] = [];
+            unset($whereData['create_time']);
         }
 
         if (!empty($data['nickname'])) {
             $whereData['nickname'] = $data['nickname'];
         } else {
-            $whereData['nickname'] = [];
+            unset($whereData['nickname']);
         }
         // 获取数据
         $feedback = model('Feedback')->getFeedback($whereData);
@@ -45,69 +45,4 @@ class FeedbackController extends BaseController
         ]);
     }
 
-
-    /**
-     * 获取反馈类型列表
-     * 可根据类型名来查询对应反馈类型
-     * @return mixed
-     */
-    public function typeIndex()
-    {
-        $this->model = 'FeedbackType';
-        $data = input('param.');
-
-        $whereData = [];
-
-        if (!empty($data['title'])) {
-            $whereData['feedback_type_name'] = ['like', '%'.$data['type_name'].'%'];
-        }
-        // 获取数据
-        $feedback_type = model('FeedbackType')->getFeedbackType($whereData);
-
-        return $this->fetch('type_index', [
-            'feedback_type'      => $feedback_type,
-        ]);
-    }
-
-    /**
-     * 添加反馈类型方法
-     * @return mixed
-     */
-    public function addType()
-    {
-        if (request()->isPost()) {
-            $data = input('post.');
-
-            // 数据需要做校验
-            $validate = validate('Feedback')->scene('addType');
-            if (!$validate->check($data)) {
-                $this->error($validate->getError());
-            }
-
-            // 获取session
-            $user = session(config('admin.session_user'), '', config('admin.session_user_scope'));
-
-            //TODO 将图片路径改为http：//的格式
-            $data = array(
-                'type_name'         => $data['title'],
-                // 'status'            => 0,         status字段数据库默认值为0，表示未启用
-            );
-
-            // 入库操作
-            try {
-                $id = model('Feedback')->add($data);
-            }catch (\Exception $e) {
-                return $this->result('', config('code.FAILURE'), '新增失败');
-            }
-
-            if ($id) {
-                return $this->result(['jump_url' => url('theme/index')], config('code.SUCCESS'), 'OK');
-            }else {
-                return $this->result('', config('code.FAILURE'), '新增失败');
-            }
-
-        }else {
-            return $this->fetch('index');
-        }
-    }
 }
