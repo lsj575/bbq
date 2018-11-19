44fd7a7b90d39da7816540849d53ed8e832bfe52
sijie.long <768712778@qq.com>
2018-11-19 19:55:37 +0800

    feat:新增测试用控制器

--- application/api/controller/TestController.php
+++ application/api/controller/TestController.php
@@ -5,3 +5,24 @@
  * Date: 2018/11/5
  * Time: 11:05
  */
+namespace app\api\controller;
+
+use app\common\lib\Aes;
+use think\Controller;
+
+class TestController extends Controller
+{
+    public function encrypt()
+    {
+        $aes = new Aes();
+        $encrypt = $aes->encrypt(input('str'));
+        echo $encrypt;
+    }
+
+    public function decrypt()
+    {
+        $aes = new Aes();
+        $decrypt = $aes->decrypt(input('str'));
+        echo $decrypt;
+    }
+}
