033c5f393aaa1285fceb9c988a7b16c7e8a34f45
sijie.long <768712778@qq.com>
2018-11-19 11:18:48 +0800

    feat:新增个人中心的各种接口

--- application/api/controller/v1/UserController.php
+++ application/api/controller/v1/UserController.php
@@ -1,27 +1,104 @@
 <?php
 namespace app\api\controller\v1;
 
-use app\api\controller\CommonController;
+use app\common\lib\Aes;;
 
 /**
- * 客户端Auth登录基础类库
- * 每个接口（需要登录的，个人中心，点赞，评论）都需要继承
- * 判断access_user_token是合法
- * 用户信息-》user
- * Class AuthBaseController
+ * 用户类，获取修改用户信息等功能
+ * Class UserController
  * @package app\api\controller\v1
  */
-class AuthBaseController extends CommonController
+class UserController extends AuthBaseController
 {
-    public function _initialize()
+    /**
+     * 获取用户信息
+     * user信息为其父类AuthBaseController获得
+     * 用户基本信息较为隐私需要加密
+     * @return \json|\think\response\Json
+     */
+    public function read()
     {
-        parent::_initialize();
+        $obj = new Aes();
+
+        return apiReturn(config('code.app_show_success'), 'ok', $obj->encrypt($this->user));
     }
 
-    public function isLogin()
+    /**
+     * 更新用户信息
+     * @return \json|\think\response\Json
+     */
+    public function update()
     {
-        if (empty($this->headers['access_user_token'])) {
-            return false;
+        $postData = input('param.');
+
+        // validate
+        $validate = validate('User');
+        if (!$validate->check($postData, [], 'User.update')) {
+            return apiReturn(config('code.app_show_error'), $validate->getError(), '', 400);
         }
+
+        //严格判断要插入的数据
+        $data = [];
+        if (!empty($postData['avatar'])) {
+            $data['avatar'] = $postData['avatar'];
+        }
+        if (!empty($postData['nickname'])) {
+            $data['nickname'] = $postData['nickname'];
+        }
+        if (!empty($postData['signature'])) {
+            $data['signature'] = $postData['signature'];
+        }
+        if (!empty($postData['home_img'])) {
+            $data['home_img'] = $postData['home_img'];
+        }
+
+        if (empty($data)) {
+            return apiReturn(config('code.app_show_error'), '数据不合法', [], 404);
+        }
+
+        try {
+            $id = model('User')->save($data, ['id' => $this->user->id]);
+            if ($id) {
+                return apiReturn(config('code.app_show_success'), 'ok', [], 202);
+            } else {
+                return apiReturn(config('code.app_show_error'), '更新失败', [], 401);
+            }
+        } catch (\Exception $e) {
+            return apiReturn(config('code.app_show_error'), $e->getMessage(), '', 500);
+        }
+    }
+
+    /**
+     * 检查昵称是否合法
+     * 在用户选择保存对应数据前，若有填写昵称则访问此接口提前检查昵称合法性
+     * @return \json|\think\response\Json
+     */
+    public function checkUserNicknamePass()
+    {
+        if (!request()->isGet()) {
+            return apiReturn(config('code.app_show_error'), '您没有权限', '', 403);
+        }
+
+        $getData = input('get.');
+
+        // validate
+        $validate = validate('User');
+        if (!$validate->check($getData, [], 'User.checkUserNicknamePass')) {
+            return apiReturn(config('code.app_show_error'), $validate->getError(), '', 400);
+        }
+
+        try {
+            $count = model('User')->where(['nickname' => $getData['nickname']])->count();
+        } catch (\Exception $e) {
+            return apiReturn(config('code.app_show_error'), $e->getMessage(), '', 500);
+        }
+
+
+        if ($count) {
+            return apiReturn(config('code.app_show_success'), '昵称符合要求', [], 202);
+        } else {
+            return apiReturn(config('code.app_show_error'), '昵称已存在', [], 401);
+        }
+
     }
 }
