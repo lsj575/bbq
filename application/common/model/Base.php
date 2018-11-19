6ff1cf84603ce9e770ff793bf3bf4746f81845e6
sijie.long <768712778@qq.com>
2018-11-19 11:11:03 +0800

    feat:新增根据条件获取数据的统计数量公共模型方法

--- application/common/model/Base.php
+++ application/common/model/Base.php
@@ -20,4 +20,19 @@ class Base extends Model
 
         return $this->id;
     }
+
+    /**
+     * 根据条件来获取列表的数据总数
+     * @param array $condition
+     * @return int|string
+     */
+    public function getCountByCondition($condition = [])
+    {
+        $condition['status'] = [
+            'neq', config('code.status_delete'),
+        ];
+
+        return $this->where($condition)
+            ->count();
+    }
 }
