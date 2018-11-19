86803bc3d188c3ac99871b04016983fc37f28573
sijie.long <768712778@qq.com>
2018-11-19 11:17:28 +0800

    feat:增加手机验证码validate

--- application/common/validate/Identify.php
+++ application/common/validate/Identify.php
@@ -3,12 +3,10 @@ namespace app\common\validate;
 
 use think\Validate;
 
-class Theme extends Validate
+class Identify extends Validate
 {
     //提交规则，|号之间不能加多余符号，包括空格
     protected $rule = [
-        'title'       => 'require|max:30',
-        'description' => 'require',
-        'image'       => 'require',
+        'id'       => 'require|number|length:11',
     ];
 }
