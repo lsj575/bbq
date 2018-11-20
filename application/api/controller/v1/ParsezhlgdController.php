932c080c022db1c3be68e8adf98c1e370ad00f72
sijie.long <768712778@qq.com>
2018-11-20 11:55:30 +0800

    feat:新增解析zhlgd首页方法

--- application/api/controller/v1/ParsezhlgdController.php
+++ application/api/controller/v1/ParsezhlgdController.php
@@ -5,3 +5,32 @@
  * Date: 2018/11/20
  * Time: 9:13 AM
  */
+namespace app\api\controller\v1;
+
+use app\api\controller\CommonController;
+
+class ParsezhlgdController extends CommonController
+{
+    /**
+     * 解析zhlgd首页获取lt和post值
+     * @return \json
+     */
+    public function parseIndex()
+    {
+        $url = 'http://zhlgd.whut.edu.cn';
+        try {
+            $html = file_get_html($url);
+        } catch (\Exception $e) {
+            return apiReturn(config('code.FAILURE'), $e->getMessage(), '', 500);
+        }
+
+        $lt = $html->find('input[id=lt]', 0)->value;
+        $postUrl =  $html->find('form[id=loginForm]', 0)->action;
+        $data = [
+            'lt'        => $lt,
+            'postUrl'   => $postUrl,
+        ];
+        return apiReturn(config('code.SUCCESS'), 'ok', $data, 200);
+    }
+}
+
