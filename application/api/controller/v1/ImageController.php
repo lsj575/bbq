b2dc62f3732a00e0f3eabe83a7164e59dbabdaee
827752806 <827752806@qq.com>
2018-11-22 08:31:06 +0800

    【fix】修改accesstoken计算方法

--- application/api/controller/v1/ImageController.php
+++ application/api/controller/v1/ImageController.php
@@ -61,7 +61,7 @@ class ImageController extends AuthBaseController
 
         // 开始生成accessToken
         $nonce = $this->createNonce(16);
-        $time = time() / 10000;
+        $time = substr(time(), 0, 6);
         $appCodeArray = str_split(config('code.APP_CODE'), 32);
         $accessToken = md5($appCodeArray[0] . $time . $appCodeArray[1] . $nonce . 'fuckjwc_token123');
 
