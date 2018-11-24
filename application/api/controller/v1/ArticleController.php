26f082a8998b5e869efca20df2aed8e079bf55e8
sijie.long <768712778@qq.com>
2018-11-24 13:06:05 +0800

    feat:新增发布动态方法（带测试）

--- application/api/controller/v1/ArticleController.php
+++ application/api/controller/v1/ArticleController.php
@@ -5,3 +5,108 @@
  * Date: 2018/8/13
  * Time: 15:52
  */
+namespace app\api\controller\v1;
+
+use app\api\controller\CommonController;
+use app\common\lib\exception\ApiException;
+
+class ArticleController extends CommonController
+{
+    public function getOneArticle()
+    {
+        if (request()->isGet()) {
+            $data = input('get');
+
+            $validate = validate('Article');
+            if (!$validate->check($data, [], 'Article.getOne')) {
+                return apiReturn(config('code.app_show_error'), $validate->getError(), '', 400);
+            }
+
+            $data['status'] = [
+                'eq', config('code.status_normal')
+            ];
+            try {
+                $articles = model('Article')->where($data)->select();
+            }catch (\Exception $e) {
+                throw new ApiException($e->getMessage(), 500);
+            }
+
+            $result = [];
+            foreach ($articles as $key => $article) {
+                $result[] = [
+                    'theme_id'   => $['id'],
+                    'theme_name' => $['theme_name'],
+                    'img_url'    => $['img'],
+                ];
+            }
+
+            return apiReturn(config('code.app_show_success'), 'OK', $result, 200);
+        }
+    }
+
+    /**
+     * 发布动态，需要提升权限要求到登录用户权限
+     * @return \json
+     * @throws ApiException
+     */
+    public function save()
+    {
+        if (request()->isPost()) {
+            // 提升权限要求
+            $auth = new AuthBaseController();
+
+            $param = input('post.');
+
+            // validate
+            $validate = validate('article');
+            if (!$validate->check($param, [], 'save')) {
+                return apiReturn(config('code.app_show_error'), $validate->getError(), '', 400);
+            }
+
+            // 文字内容和图片不能全为空
+            if ($param['img'] || $param['content']) {
+                $data = [
+                    'theme_id'          => $param['theme_id'],
+                    'user_id'           => $auth->user->id,
+                    'content'           => is_null($param['content']) ? '' : $param['content'],
+                    'img'               => is_null($param['img']) ? '' : implode($param['img'], ','),
+                    'allow_watermark'   => $param['img_watermark'],
+                    'allow_comment'     => $param['allow_comment'],
+                ];
+            }
+            try {
+                $id = model('article')->add($data);
+            } catch (\Exception $e) {
+                throw new ApiException($e->getMessage(), 500, config('code.app_show_error'));
+            }
+
+            if ($id) {
+                return apiReturn(config('code.app_show_success'), 'ok', '', 200);
+            } else {
+                return apiReturn(config('code.app_show_error'), '发布动态失败', '', 500);
+            }
+        }
+    }
+
+    /**
+     * 判断是否存在某个id的文章且状态正常
+     * @param $id
+     * @return bool
+     * @throws ApiException
+     */
+    public function isExistsTheArticle($id)
+    {
+        try {
+            $count = model('Article')->getNormalArticleCount($id);
+        } catch (\Exception $e) {
+            throw new ApiException($e->getMessage(), 500);
+        }
+
+        if ($count) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
