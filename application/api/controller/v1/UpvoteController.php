78634cec28b54084c12c9eb18477f6066072239b
sijie.long <768712778@qq.com>
2018-11-27 19:26:21 +0800

    feat:新增取消点赞功能

--- application/api/controller/v1/UpvoteController.php
+++ application/api/controller/v1/UpvoteController.php
@@ -48,9 +48,52 @@ class UpvoteController extends AuthBaseController
                 }
                 $userArticleId = model('UserArticles')->add($data);
                 if ($userArticleId) {
+                    model('Article')->where(['id' => $id])->setInc('likes');
                     return apiReturn(config('code.app_show_success'), 'OK', [], 202);
                 } else {
                     return apiReturn(config('code.app_show_error'), '内部错误，点赞失败', [], 500);
+                }
+            } catch (\Exception $e) {
+                return apiReturn(config('code.app_show_error'), $e->getMessage(), [], 500);
+            }
+        } else {
+            return apiReturn(config('code.app_show_error'), '不存在该文章', [], 403);
+        }
+    }
+
+    public function delete()
+    {
+        $id = input('delete.id', 0, 'intval');
+
+        if (!$id) {
+            return apiReturn(config('code.app_show_error'), 'id不存在', [], 404);
+        }
+
+        // 判断此id的文章是否存在，且状态是否正常
+        try {
+            $article = model('Article')->get(['id' => $id, 'status' => config('code.status_normal')]);
+        } catch (\Exception $e) {
+            throw new ApiException($e->getMessage(), 500);
+        }
+
+        if ($article) {
+            $data = [
+                'user_id' => $this->user->id,
+                'article_id' => $id,
+            ];
+
+            try {
+                // 查询数据库中是否存在点赞
+                $userArticle = model('UserArticles')->get($data);
+                if (empty($userArticle)) {
+                    return apiReturn(config('code.app_show_error'), '没有被点赞过，无法取消', [], 401);
+                }
+                $userArticleId = model('UserArticles')->where($data)->delete();
+                if ($userArticleId) {
+                    model('Article')->where(['id' => $id])->setDec('likes');
+                    return apiReturn(config('code.app_show_success'), 'OK', [], 202);
+                } else {
+                    return apiReturn(config('code.app_show_error'), '内部错误，取消点赞失败', [], 500);
                 }
             } catch (\Exception $e) {
                 return apiReturn(config('code.app_show_error'), $e->getMessage(), [], 500);
