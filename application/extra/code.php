4f45575421a1eca34dc92771576ffe56b094563e
sijie.long <768712778@qq.com>
2018-11-29 10:29:59 +0800

    feat:新增推荐状态code

--- application/extra/code.php
+++ application/extra/code.php
@@ -20,5 +20,6 @@ return [
     'user_normal'           => 1,  //用户正常
     'user_delete'           => -1,  //用户被删除
     'user_ban'              => 0,  //用户被禁止
+    'is_position'           => 1, // 推荐状态
     'APP_CODE'              => 'Yf6u0XI8yVOx5OxSM7iyfXckGmtYNfuxI8CxmcE1ghHOcyGMrp5SCdouoQmV51qq', //静态资源文件的bbq专属code
 ];
