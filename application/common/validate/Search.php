9743d076126051c028ad086fe9a6d7bd0e54bae1
sijie.long <768712778@qq.com>
2018-12-01 11:55:14 +0800

    feat:新增搜索validate

--- application/common/validate/Search.php
+++ application/common/validate/Search.php
@@ -3,12 +3,10 @@ namespace app\common\validate;
 
 use think\Validate;
 
-class Theme extends Validate
+class Search extends Validate
 {
     //提交规则，|号之间不能加多余符号，包括空格
     protected $rule = [
-        'title'       => 'require|max:30',
-        'description' => 'require',
-        'image'       => 'require',
+        'search_str'    => 'require|max:40',
     ];
 }
