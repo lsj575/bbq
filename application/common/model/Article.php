87904afc02f0b3456398911c89c41f2313e08b98
sijie.long <768712778@qq.com>
2018-12-02 10:19:02 +0800

    feat:封装查询的字段

--- application/common/model/Article.php
+++ application/common/model/Article.php
@@ -125,7 +125,7 @@ class Article extends Base
 
         return $this->table($this->table)
             ->alias('a')
-            ->field('a.img as img, t.img as theme_img, a.id as id, u.id as user_id, t.id as theme_id, content, avatar, nickname, likes, comments, theme_name, a.create_time, a.is_position as is_position')
+            ->field($this->_getListField())
             ->join('user u', 'u.id = a.user_id')
             ->join('theme t', 't.id = a.theme_id')
             ->where($whereData)
@@ -156,7 +156,7 @@ class Article extends Base
 
         return $this->table($this->table)
             ->alias('a')
-            ->field('a.img as img, t.img as theme_img, a.id as article_id, u.id as user_id, t.id as theme_id, content, u.avatar as user_avatar, u.nickname as user_nickname, likes, comments, theme_name, a.is_position as is_position, a.create_time')
+            ->field($this->_getListField())
             ->join('user u', 'u.id = a.user_id')
             ->join('theme t', 't.id = a.theme_id')
             ->where($whereData)
@@ -183,7 +183,7 @@ class Article extends Base
 
         return $this->table($this->table)
             ->alias('a')
-            ->field('a.img as img, t.img as theme_img, a.id as id, u.id as user_id, t.id as theme_id, content, u.avatar as user_avatar, u.nickname as user_nickname, likes, comments, theme_name, a.create_time, a.is_position as is_position')
+            ->field($this->_getListField())
             ->join('user u', 'u.id = a.user_id')
             ->join('theme t', 't.id = a.theme_id')
             ->where($whereData)
@@ -197,12 +197,19 @@ class Article extends Base
     private function _getListField()
     {
         return [
-            'id',
-            'user_id',
-            'theme_id',
-            'content',
+            'a.img as img',
+            't.img as theme_img',
+            'a.id as id',
+            'u.id as user_id',
+            't.id as theme_id',
+            'a.is_position as is_position',
+            'u.avatar as user_avatar',
+            'u.nickname as user_nickname',
+            'a.create_time',
             'likes',
-            'read_count',
+            'comments',
+            'theme_name',
+            'content',
         ];
     }
 
