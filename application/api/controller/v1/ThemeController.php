8ae04b141062912ff6bba1f15661df3a410446ec
sijie.long <768712778@qq.com>
2018-12-03 11:41:20 +0800

    feat:新增返回值字段

--- application/api/controller/v1/ThemeController.php
+++ application/api/controller/v1/ThemeController.php
@@ -25,9 +25,10 @@ class ThemeController extends CommonController
         $result = [];
         foreach ($themes as $key => $theme) {
             $result[] = [
-                'theme_id'   => $theme['id'],
-                'theme_name' => $theme['theme_name'],
-                'img_url'    => $theme['img'],
+                'theme_id'              => $theme['id'],
+                'theme_name'            => $theme['theme_name'],
+                'theme_introduction'    => $theme['theme_introduction'],
+                'img_url'               => $theme['img'],
             ];
         }
 
@@ -52,14 +53,47 @@ class ThemeController extends CommonController
             $result = [];
             foreach ($themes as $key => $theme) {
                 $result = [
-                    'theme_id'      => $theme['id'],
-                    'theme_img'     => $theme['img'],
-                    'theme_name'    => $theme['theme_name'],
+                    'theme_id'              => $theme['id'],
+                    'theme_img'             => $theme['img'],
+                    'theme_name'            => $theme['theme_name'],
+                    'theme_introduction'    => $theme['theme_introduction'],
                 ];
             }
             return apiReturn(config('code.app_show_success'), 'OK', $result, 200);
         } else {
             return apiReturn(config('code.app_show_error'), 'error', [], 403);
+        }
+    }
+
+    /**
+     * 获取推荐主题
+     * @return \json
+     * @throws ApiException
+     */
+    public function getRecommendThemes()
+    {
+        if (request()->isGet()) {
+            $offset = input('param.offset', 0, 'intval');
+
+            try {
+                $adminRecommendThemes = model('theme')->getRecommendThemes($offset);
+            } catch (\Exception $e) {
+                throw new ApiException($e->getMessage(), 500);
+            }
+
+            $result = [];
+            foreach ($adminRecommendThemes as $key => $adminRecommendTheme) {
+                $result = [
+                    'theme_id'              => $adminRecommendTheme['id'],
+                    'theme_img'             => $adminRecommendTheme['img'],
+                    'theme_name'            => $adminRecommendTheme['theme_name'],
+                    'theme_introduction'    => $adminRecommendTheme['theme_introduction'],
+                ];
+            }
+
+            return apiReturn(config('code.app_show_success'), 'OK', $result, 200);
+        } else {
+            return apiReturn(config('code.app_show_error'), '请求不合法', [], 403);
         }
     }
 }
--- application/common/model/Article.php
+++ application/common/model/Article.php
@@ -235,6 +235,7 @@ class Article extends Base
             'u.avatar as user_avatar',
             'u.nickname as user_nickname',
             'a.create_time',
+            'theme_introduction',
             'likes',
             'comments',
             'theme_name',
