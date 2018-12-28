2441465ad6a1ace69d8ad19c1a60e8470a0b574f
shudal <shudal@qq.com>
2018-12-28 20:41:13 +0800

    完成评论管理

---
+++ application/admin/controller/CommentController.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Junhao
+ * Date: 2018/12/14
+ * Time: 21:26
+ */
+
+namespace app\admin\controller;
+
+use app\common\model\ArticleComment;
+use think\Controller;
+use app\common\lib\IAuth;
+class CommentController extends BaseController
+{
+  public $model="ArticleComment";
+  public function index(){
+    $this->_initialize();
+      if(request()->isGet()){
+            $data = request()->param();
+
+            if(!empty($data["thetext"]) && $data["thetext"]!=""){
+              if($data["condition"]=="content"){//按内容搜索
+                $comments = model("ArticleComment")->where("content","like","%".$data["thetext"]."%")->paginate(5);
+                $this->assign("page",$comments->render());
+              }
+              else if($data["condition"]=="commentator"){//按评论者
+                $users = model("User")->where("nickname","like","%".$data["thetext"]."%")->select();
+                $comments=array();
+                foreach($users as $user){
+                  $comments = $comments + model("ArticleComment")->where("user_id","=",$user->id)->select();
+                }
+                $start = empty($data["start"])? 0:$data["start"];
+                $comments = array_slice($comments,$start,5);
+                $this->assign("len",count($comments));
+              }
+              else{//按被评论者
+                $users = model("User")->where("nickname","like","%".$data["thetext"]."%")->select();
+                $comments =array();
+                foreach($users as $user){
+                  $comments = $comments + model("ArticleComment")->where("to_user_id","=",$user->id)->select();
+                }
+                $start = empty($data["start"])? 0:$data["start"];
+                $comments = array_slice($comments,$start,5);
+                $this->assign("len",count($comments));
+              }
+            }
+            else{
+                $comments = model("ArticleComment")::paginate(5);
+                $this->assign("page",$comments->render());
+            }
+            if(count($comments)){
+              foreach($comments as $comment){
+                $comment["user_id"]=model("User")::get($comment->user_id)->nickname;
+                $comment["to_user_id"]=model("User")::get($comment->to_user_id)->nickname;
+              }
+            }
+            //dump($comments);
+            $this->assign("comments",$comments);
+        }
+        return $this->fetch();
+  }
+/*
+  public function delete($id=0){
+    $this->_initialize();
+    $isLogin = $this->isLogin();
+    if($isLogin){
+      if(request()->isPost()){
+        $comment = model("ArticleComment")->get(request()->param("id"));
+        $comment->delete();
+        return "删除成功";
+
+      }
+    }
+    else return $this->redirect("index/index");
+  }
+   */
+}
