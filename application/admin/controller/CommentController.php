87b8521de8357e50a2d6fff7087fc81a437c51bf
shudal <shudal@qq.com>
2019-01-18 14:35:57 +0800

    重构评论管理控制器

--- application/admin/controller/CommentController.php
+++ application/admin/controller/CommentController.php
@@ -13,43 +13,22 @@ use think\Controller;
 use app\common\lib\IAuth;
 class CommentController extends BaseController
 {
-    public $model="ArticleComment";
     public function index(){
         if(request()->isGet()){
+            $this->model = 'ArticleComment';
             $data = input('get.');
-            //有搜索内容时时执行
-            if(!empty($data["thetext"]) && $data["thetext"]!=""){
-                if($data["condition"]=="content"){//按内容搜索
-                    $comments = model("ArticleComment")->where("content","like","%".$data["thetext"]."%")->paginate(5);
-                    $this->assign("page",$comments->render());
-                }else if($data["condition"]=="commentator"){//按评论者
-                    $comments = model("User")
-                        ->alias('u')
-                        ->join('article_comment m','u.id = m.user_id')
-                        ->where('nickname','like','%'.$data['thetext'].'%')
-                        ->paginate(5);
-                    $this->assign("page",$comments->render());
-                }else{//按被评论者
-                    $comments = model("User")
-                        ->alias('u')
-                        ->join('article_comment m','u.id = m.to_user_id')
-                        ->where('nickname','like','%'.$data['thetext'].'%')
-                        ->paginate(5);
-                    $this->assign("page",$comments->render());
-                }
-            }else{//当没有搜索内容时执行
-                $comments = model("ArticleComment")::paginate(5);
-                //这里的分页是tp框架自带的
-                $this->assign("page",$comments->render());
+
+            $whereData = [];
+            if(!empty($data['content'])){
+                $whereData['content'] = ['like',"%".$data['content']."%"];
             }
-            if(count($comments)){
-                foreach($comments as $comment){
-                    $comment["user_id"]=model("User")::get($comment->user_id)->nickname;
-                    $comment["to_user_id"]=model("User")::get($comment->to_user_id)->nickname;
-                }
-            } 
-            $this->assign("comments",$comments);
+            if(!empty($data['nickname'])){
+                $whereData['nickname'] = ['like',"%".$data['nickname']."%"];
+            }
+            if(empty($data['identity'])) $data['identity']='commentator';
+            
+            $comments = model('ArticleComment')->getComment($whereData,$data['identity']);
+            return $this->fetch('',['comments'=>$comments,'page'=>$comments->render()]); 
         }
-        return $this->fetch();
     }
 }
--- application/common/model/ArticleComment.php
+++ application/common/model/ArticleComment.php
@@ -11,6 +11,33 @@ class ArticleComment extends Base
      * @param array $param
      * @return int|string
      */
+    protected $table = 'article_comment';
+    public function getComment($data=[],$identity){
+        if(isset($data['content'])){
+            $data['m.content'] = $data['content'];
+            unset($data['content']);
+        }
+        if(isset($data['nickname'])){
+            $data['u.nickname'] = $data['nickname'];
+            unset($data['nickname']);
+        }
+
+        if($identity=='commentator')
+            $identity='u.id = m.user_id';
+        else $identity='u.id = m.to_user_id';
+        $results = model("User")
+            ->alias('u')
+            ->join("$this->table m",$identity)
+            ->where($data)
+            ->paginate(5);
+        if(count($results)){
+            foreach($results as $result){
+                $result['user_id']= model("User")::get($result->user_id)->nickname;
+                $result['to_user_id']=model("User")::get($result->to_user_id)->nickname;
+            }
+        }
+        return $results;
+    }
     public function getNormalCommentsCountByCondition($param = [])
     {
         $param['status'] = config('code.user_normal');
