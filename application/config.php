91b5a9d146ef96c2f061e9b3fac95cdce46e6f6b
sijie.long <768712778@qq.com>
2019-02-03 13:31:05 +0800

    fix:视图字符串替换bug

--- application/config.php
+++ application/config.php
@@ -140,7 +140,8 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__STATIC__'    => SCRIPT_DIR . '/static',
+        // '__STATIC__'    => SCRIPT_DIR . '/static',
+        '__STATIC__'    => [],
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
