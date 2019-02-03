0d629bb664b8dd247ed5cf970b09449ba7558000
sijie.long <768712778@qq.com>
2019-02-03 11:45:00 +0800

    feat:更改提交字段的校验

--- application/common/validate/SlideImg.php
+++ application/common/validate/SlideImg.php
@@ -8,7 +8,7 @@ class SlideImg extends Validate
     //提交规则，|号之间不能加多余符号，包括空格
     protected $rule = [
         'description'   => 'require',
-        'img'           => 'require',
+        'image'         => 'require',
         'img_type'      => 'require',
     ];
 }
