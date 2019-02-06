b1b94291c3b3928f57c8a55d5d1757ee103c400d
sijie.long <768712778@qq.com>
2019-02-06 17:09:31 +0800

    feat:新增获取用户收藏列表接口

--- application/api/controller/v1/CollectionController.php
+++ application/api/controller/v1/CollectionController.php
@@ -26,7 +26,7 @@ class CollectionController extends AuthBaseController
         if (!$id) {
             return apiReturn(config('code.app_show_error'), 'id不存在', [], 404);
         }
-        // 判断此id的主题是否存在，且状态是否正常
+        // 判断此id的动态是否存在，且状态是否正常
         try {
             $article = model('article')->get(['id' => $id, 'status' => config('code.status_normal')]);
         } catch (\Exception $e) {
@@ -40,12 +40,12 @@ class CollectionController extends AuthBaseController
             ];
             model('UserCollection')->startTrans();
             try {
-                // 查询数据库中是否存在该关注
+                // 查询数据库中是否存在该收藏
                 $userCollection = model('UserCollection')->get($data);
                 if ($userCollection) {
                     return apiReturn(config('code.app_show_error'), '已收藏,请勿重复收藏', [], 401);
                 }
-                // 未被关注
+                // 未被收藏
                 $userCollectionId = model('UserCollection')->add($data);
                 if ($userCollectionId) {
                     model('UserCollection')->commit();
@@ -113,7 +113,7 @@ class CollectionController extends AuthBaseController
 
 
     /**
-     * 获取主题是否被某用户关注
+     * 获取动态是否被某用户收藏
      * @return \json
      * @throws ApiException
      */
@@ -148,8 +148,60 @@ class CollectionController extends AuthBaseController
                 throw new ApiException($e->getMessage(), 500);
             }
         } else {
-            return apiReturn(config('code.app_show_error'), '不存在该主题', [], 403);
+            return apiReturn(config('code.app_show_error'), '不存在该动态', [], 403);
         }
     }
 
+    /**
+     * 获取用户收藏的动态
+     * @return \json
+     */
+    public function getArticleOfUserCollect()
+    {
+        if (request()->isGet()) {
+            try {
+                $articles = model('UserCollection')->getArticleOfUserCollect($this->user->id);
+            } catch (\Exception $e) {
+                return apiReturn(config('code.app_show_error'), $e->getMessage(), '', 500);
+            }
+
+            // 整理数据
+            $result = [];
+            if ($articles) {
+                foreach ($articles as $key => $article) {
+                    $result[] = $this->organizeDataOfArticle($article);
+                }
+            }
+
+            return apiReturn(config('code.app_show_success'), 'OK', $result, 200);
+        } else {
+            return apiReturn(config('code.app_show_error'), 'error', [], 403);
+        }
+    }
+
+    /**
+     * 整理动态数据
+     * @param $article
+     * @return array
+     */
+    private function organizeDataOfArticle($article)
+    {
+        return [
+            'user_id'               => $article['user_id'],
+            'article_id'            => $article['id'],
+            'theme_id'              => $article['theme_id'],
+            'theme_name'            => $article['theme_name'],
+            'theme_introduction'    => $article['theme_introduction'],
+            'content'               => $article['content'],
+            'img'                   => $article['img'] == "" ? "" : explode($article['img'], ','),
+            'theme_img'             => $article['theme_img'],
+            'likes'                 => $article['likes'],
+            'comments'              => $article['comments'],
+            'user_nickname'         => $article['user_nickname'],
+            'user_avatar'           => $article['user_avatar'],
+            'user_signature'        => $article['signature'],
+            'is_position'           => $article['is_position'],
+            'create_time'           => $article['create_time'],
+        ];
+    }
 }
--- application/common/model/UserCollection.php
+++ application/common/model/UserCollection.php
@@ -12,5 +12,57 @@ namespace app\common\model;
  */
 class UserCollection extends Base
 {
+    protected $table = 'user_collection';
+
+    /**
+     * 获取用户收藏的动态
+     * @param $user_id
+     * @return false|\PDOStatement|string|\think\Collection
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getArticleOfUserCollect($user_id)
+    {
+        $whereData = [
+            'a.status'      => config('code.status_normal'),
+            't.status'      => config('code.status_normal'),
+            'uc.user_id'    => $user_id,
+        ];
+
+        return $this->table($this->table)
+            ->alias('uc')
+            ->field($this->_getListField())
+            ->join('article a', 'a.id = uc.article_id')
+            ->join('user u', 'u.id = a.user_id')
+            ->join('theme t', 't.id = a.theme_id')
+            ->where($whereData)
+            ->select();
+    }
+
+    /**
+     * 通用化获取参数的数据字段
+     * @return array
+     */
+    private function _getListField()
+    {
+        return [
+            'a.img as img',
+            't.img as theme_img',
+            'a.id as id',
+            'a.user_id as user_id',
+            't.id as theme_id',
+            'a.is_position as is_position',
+            'u.avatar as user_avatar',
+            'u.nickname as user_nickname',
+            'u.signature as signature',
+            'a.create_time',
+            'theme_introduction',
+            'likes',
+            'comments',
+            'theme_name',
+            'content',
+        ];
+    }
 
 }
