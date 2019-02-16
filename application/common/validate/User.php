895323866f64552d9b343f136f319eb8dd59eb74
sijie.long <768712778@qq.com>
2019-02-16 10:56:10 +0800

    feat:将昵称改为更新用户信息和检查用户昵称合法性的必传字段

--- application/common/validate/User.php
+++ application/common/validate/User.php
@@ -7,7 +7,7 @@ class User extends Validate
 {
     //提交规则，|号之间不能加多余符号，包括空格
     protected $rule = [
-        'nickname'  => 'chsDash|max:30',
+        'nickname'  => 'require|chsDash|max:30',
         'signature' => 'max:200',
         'avatar'    => 'max:255',
         'home_img'  => 'max:255',
@@ -16,6 +16,6 @@ class User extends Validate
     //应用场景
     protected $scene = [
         'update'   =>  ['nickname', 'signature', 'avatar', 'home_img'],
-        'checkUserNicknamePass' => ['nickname' => 'require|max:30',]
+        'checkUserNicknamePass' => ['nickname',]
     ];
 }
